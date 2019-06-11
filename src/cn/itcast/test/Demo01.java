847446ffdd6debaa732794d2c5854d3436556ca6
chacha <3065319417@qq.com>
2019-06-11 18:08:37 +0800

    Git 存到本地仓库。。。

---
+++ src/cn/itcast/test/Demo01.java
@@ -0,0 +1,9 @@
+package cn.itcast.test;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("git........");
+        System.out.println("git........");
+        System.out.println("git........");
+    }
+}
